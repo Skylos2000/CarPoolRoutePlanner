cbd99189f92f7a0a97ff3eba26c0275c2b12b811
Lep021 <62142944+Lep021@users.noreply.github.com>
2021-07-25 13:25:19 -0500

    Update Manifest

--- app/src/main/java/com/devs/carpoolrouteplanner/MainActivity.kt
+++ app/src/main/java/com/devs/carpoolrouteplanner/MainActivity.kt
@@ -12,15 +12,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         var email = findViewById(R.id.email) as EditText
         var password = findViewById(R.id.password) as EditText
+
         val button: Button = findViewById(R.id.button)
         val intent = Intent(this@MainActivity, SecondActivity::class.java)
 
         button.setOnClickListener{
             val username = email.text
             val code = password.text
-            if(username.toString().equals("hello") && code.toString().equals("world")){
+
+            if(username.toString().equals("") && code.toString().equals("")){
                 Toast.makeText(this, "Log In Successful", Toast.LENGTH_SHORT).show()
                 startActivity(intent)
             }
