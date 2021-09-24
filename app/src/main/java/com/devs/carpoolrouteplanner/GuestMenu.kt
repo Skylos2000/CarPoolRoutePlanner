9de6483173174d063713d5dadd025393b90a1ccc
Manish Kad <manishkadayat@gmail.com>
2021-09-24 12:58:26 +0545

    Invite others button now actually retrieves invite code from the database, for testing it fetches or creates invite code for group with gid 123

--- app/src/main/java/com/devs/carpoolrouteplanner/GuestMenu.kt
+++ app/src/main/java/com/devs/carpoolrouteplanner/GuestMenu.kt
@@ -9,6 +9,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
 import com.devs.carpoolrouteplanner.utils.getConfigValue
 import io.ktor.client.*
@@ -16,6 +17,8 @@ import io.ktor.client.engine.cio.*
 import io.ktor.client.features.auth.*
 import io.ktor.client.features.auth.providers.*
 import io.ktor.client.request.*
+import io.ktor.client.request.forms.*
+import io.ktor.http.*
 import kotlinx.coroutines.launch
 
 class GuestMenu : AppCompatActivity() {
@@ -27,7 +30,7 @@ class GuestMenu : AppCompatActivity() {
         val button2: Button = findViewById(R.id.button2)
         val button3: Button = findViewById(R.id.button3)
         val inviteOthers: Button = findViewById(R.id.inviteOthers)
-        val deleteGroup:Button = findViewById<Button>(R.id.button6)
+        val deleteGroup: Button = findViewById<Button>(R.id.button6)
 
         val intent1 = Intent(this@GuestMenu, ViewCurrentMembers::class.java)
         val intent2 = Intent(this@GuestMenu, GuestLeaveMembers::class.java)
@@ -35,7 +38,7 @@ class GuestMenu : AppCompatActivity() {
 
         val gid = intent.getStringExtra("GID")
         button1.setOnClickListener {
-            intent1.putExtra("GID",gid)
+            intent1.putExtra("GID", gid)
             startActivity(intent1)
         }
 
@@ -47,18 +50,60 @@ class GuestMenu : AppCompatActivity() {
             startActivity(intent3)
         }
 
-        deleteGroup.setOnClickListener{
+        deleteGroup.setOnClickListener {
             showdialog()
         }
 
-        inviteOthers.setOnClickListener{
-            var inviteCode = "invitecode";
-            var dummyLink = "https://coolrouteplanner.test/join/${inviteCode}";
-            val shareIntent = Intent()
-            shareIntent.action = Intent.ACTION_SEND
-            shareIntent.type="text/plain"
-            shareIntent.putExtra(Intent.EXTRA_TEXT, "Please join my group using this invite code: ${inviteCode}")
-            startActivity(Intent.createChooser(shareIntent,"Send To"))
+        inviteOthers.setOnClickListener {
+            val myurl = getConfigValue("backend_url") ?: return@setOnClickListener
+            val gid = "123"
+            inviteOthers.isClickable = false
+            lifecycleScope.launch {
+
+                val client = HttpClient(CIO) {
+                    install(Auth) {
+                        basic {
+                            credentials {
+                                BasicAuthCredentials(username = AccountSignIn.creds[0],
+                                    password = AccountSignIn.creds[1])
+                            }
+
+                        }
+                    }
+                }
+
+                try {
+                    val response: String = client.submitForm (
+                        url = myurl+ "create_invite/",
+                        formParameters = Parameters.build {
+                            append("gid",gid)
+                        }
+                    )
+                    val data = response
+                    Toast.makeText(applicationContext,
+                        data,
+                        Toast.LENGTH_LONG).show()
+                    var inviteCode = data;
+                    inviteOthers.isClickable = true
+
+                    var dummyLink = "https://coolrouteplanner.test/join/${inviteCode}";
+                    val shareIntent = Intent()
+                    shareIntent.action = Intent.ACTION_SEND
+                    shareIntent.type = "text/plain"
+                    shareIntent.putExtra(Intent.EXTRA_TEXT,
+                        "Please join my group using this invite code: ${inviteCode}")
+                    startActivity(Intent.createChooser(shareIntent, "Send To"))
+
+//                    Toast.makeText(applicationContext, "Successfully created invite link.${data}", Toast.LENGTH_LONG).show()
+                } catch (E: Exception) {
+                    Toast.makeText(applicationContext,
+                        "Exception: No group with gid 123 or ${E.message}",
+                        Toast.LENGTH_LONG).show()
+                    inviteOthers.isClickable = true
+                }
+
+
+            }
         }
     }
 
@@ -99,7 +144,7 @@ class GuestMenu : AppCompatActivity() {
                     Toast.makeText(applicationContext, "Successfully performed delete action", Toast.LENGTH_LONG).show()
                     dialog.cancel()
                 }catch (E:Exception){
-                    Toast.makeText(applicationContext,"Failed to delete. Wrong Group Id?",Toast.LENGTH_LONG)
+                    Toast.makeText(applicationContext,"Failed to delete. Wrong Group Id?",Toast.LENGTH_LONG).show()
                     dialog.cancel()
                 }
             }
