ac0b6954548aaf5687d3647c613adefabffb2a74
Solomon Loche <sloche3@gmail.com>
2021-11-06 16:45:20 -0500

    Added group voting fragment

--- app/src/main/java/com/devs/carpoolrouteplanner/ui/MainGroupActivity.kt
+++ app/src/main/java/com/devs/carpoolrouteplanner/ui/MainGroupActivity.kt
@@ -3,20 +3,24 @@ package com.devs.carpoolrouteplanner.ui
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.devs.carpoolrouteplanner.R
 import com.devs.carpoolrouteplanner.databinding.ActivityMainGroupBinding
+import kotlinx.serialization.descriptors.PrimitiveKind
 
 class MainGroupActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainGroupBinding
-    //val gid = intent.extras?.getInt("groupId")
+    var gid = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        gid = intent.extras?.getInt("groupId")!!
 
         binding = ActivityMainGroupBinding.inflate(layoutInflater)
         setContentView(binding.root)
